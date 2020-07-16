3a785e1a0b23a88dc38268df2d862c9dea1707d8
Amandine FORTIER <amandinefor@gmail.com>
2020-07-16 19:15:27 +0200

    Thread server and client. Client send only one message, server rewrite it.

---
+++ client.cpp
@@ -0,0 +1,43 @@
+#include <sys/socket.h> // For socket functions
+#include <netinet/in.h> // For sockaddr_in
+#include <cstdlib> // For exit() and EXIT_FAILURE
+#include <iostream> // For cout
+#include <unistd.h> // For read
+#include <errno.h>
+
+int main()
+{
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sockfd < 0)
+    {
+        std::cout << "Failed to create socket. errno: " << errno << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    sockaddr_in sockaddr; 
+    sockaddr.sin_family = AF_INET;
+    sockaddr.sin_port = htons(9999); // htons is necessary to convert a number to
+    sockaddr.sin_addr.s_addr = INADDR_ANY;
+                                    // network byte order
+
+    if (connect(sockfd, (struct sockaddr*)&sockaddr, sizeof(sockaddr)) < 0)
+    {
+        std::cout << "Failed to connect to port 9999. errno: " << errno << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+   // Read from the connection
+    char buffer[100];
+    auto bytesRead = read(sockfd, buffer, 100);
+    std::cout<<buffer<<std::endl;
+
+    char buff[100];
+    read(1, buff, 100);
+
+    // Send a message to the connection
+    send(sockfd, buff, 100, 0);
+
+}
+
+
