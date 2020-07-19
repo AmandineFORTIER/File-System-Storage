fd464833368beec70be3c7c1463c32f78a0b90ad
Amandine FORTIER <amandinefor@gmail.com>
2020-07-19 17:04:38 +0200

    client : Msg to connect, create or quit. Add message to enter username and password --> password without echo.

--- client.cpp
+++ client.cpp
@@ -6,9 +6,29 @@
 #include <errno.h>
 #include <stdio.h>
 #include <string.h>
+#include <cstring>
+#include <termios.h>
 
 int main()
 {
+    std::cout <<" === Welcome to a file storage system ===\n"<<
+                "  To create an account write '""create""'\n"<<
+                "  To connect yourself write '""connect""'\n"<<
+                "  To quit write '""quit""'"<<std::endl;
+    
+    //traiter le quit ici le reste envoyer et le serveur gère
+    std::string s;
+    while (std::cin >> s)
+    {
+        if (std::strcmp(s.c_str(),"quit")==0)
+        {
+            exit(EXIT_SUCCESS);
+        }else if (std::strcmp(s.c_str(),"create")==0 || std::strcmp(s.c_str(),"connect")==0)
+        {
+            break;
+        }
+    }
+
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
     if (sockfd < 0)
@@ -28,6 +48,47 @@ int main()
         std::cout << "Failed to connect to port 9999. errno: " << errno << std::endl;
         exit(EXIT_FAILURE);
     }
+
+    
+    //demander username et password et après connection
+    fflush(stdin);
+    std::string username;
+    std::cout<<"Username: ";
+    std::cin >> username;
+
+    /* ignore signals */
+	signal(SIGINT, SIG_IGN);
+	signal(SIGTERM, SIG_IGN);
+    
+	/* no echo */
+	struct termios term;
+	tcgetattr(1, &term);
+	term.c_lflag &= ~ECHO;
+	tcsetattr(1, TCSANOW, &term);
+
+    
+    std::string password;
+    std::cout<<"Password: ";
+    std::cin >> password;
+    std::cout<<"\nPASS : "<<password<<std::endl;
+    
+
+	/* do whatever handling you want here.
+	 * fgetc, appending c to a char array and
+	 * realloc'ing as necessary may be safest. */
+	int c;
+	while ((c=fgetc(stdin)) != '\n');
+
+	/* reset the term */
+	term.c_lflag |= ECHO;
+	tcsetattr(1, TCSANOW, &term);
+	return 0;
+
+
+
+
+
+
 
 
    // Read from the connection
