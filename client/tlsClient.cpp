feec2c22a8d7ea3aa1d16f0a2827b6d49b028c5d
Amandine FORTIER <amandinefor@gmail.com>
2020-07-31 13:22:49 +0200

    certif OK. Some modifications on server an client tls to adapt certificat. add socket communication inside TLS, need a lot of improvements

--- client/tlsClient.cpp
+++ client/tlsClient.cpp
@@ -6,6 +6,22 @@
 #include <botan-2/botan/certstor.h>
 #include <botan-2/botan/certstor_system.h>
 
+
+
+#include "client.h"
+
+#include <sys/socket.h> // For socket functions
+#include <netinet/in.h> // For sockaddr_in
+#include <cstdlib> // For exit() and EXIT_FAILURE
+#include <unistd.h> // For read
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <cstring>
+#include <termios.h>
+
+
+
 /**
  * @brief Callbacks invoked by TLS::Channel.
  *
@@ -89,6 +105,95 @@ class Client_Credentials : public Botan::Credentials_Manager
        std::vector<Botan::Certificate_Store*> m_stores;
 };
 
+
+
+
+
+
+
+
+std::string ask_username()
+{
+    std::string username;
+    std::cout<<"Username: ";
+    std::cin >> username;
+    return username;
+}
+
+void ask_password(char password[72])
+{
+    std::cout<<"Password : ";
+
+    /* ignore signals */
+	//signal(SIGINT, SIG_IGN);
+	//signal(SIGTERM, SIG_IGN);
+    
+	/* no echo */
+	struct termios term;
+	tcgetattr(1, &term);
+	term.c_lflag &= ~ECHO;
+	tcsetattr(1, TCSANOW, &term);
+
+
+    //char pass[72];
+    std::cin>>password;
+    std::cin.clear();
+
+    
+	/* reset the term */
+	term.c_lflag |= ECHO;
+	tcsetattr(1, TCSANOW, &term);
+
+    std::cin.clear();
+    std::cout.clear();
+    std::cout<<std::endl;
+
+}
+
+
+int connect_to_server()
+{
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sockfd < 0)
+    {
+        std::cout << "Failed to create socket. errno: " << errno << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    sockaddr_in sockaddr; 
+    sockaddr.sin_family = AF_INET;
+    sockaddr.sin_port = htons(9999); // htons is necessary to convert a number to
+    sockaddr.sin_addr.s_addr = INADDR_ANY;
+                                    // network byte order
+
+    if (connect(sockfd, (struct sockaddr*)&sockaddr, sizeof(sockaddr)) < 0)
+    {
+        std::cout << "Failed to connect to port 9999. errno: " << errno << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    return sockfd;
+}
+
+void read_from_connection(int sockfd)
+{
+    // Read from the connection
+    char buffer[100];
+    auto bytesRead = read(sockfd, buffer, 100);
+    std::cout<<buffer<<std::endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
 int main()
    {
    // prepare all the parameters
@@ -104,14 +209,62 @@ int main()
                              creds,
                              policy,
                              rng,
-                             Botan::TLS::Server_Information("botan.randombit.net", 443),
+                             Botan::TLS::Server_Information("localhost", 443),
                              Botan::TLS::Protocol_Version::TLS_V12);
 
    while(!client.is_closed())
-      {
+   {
       // read data received from the tls server, e.g., using BSD sockets or boost asio
       // ...
 
       // send data to the tls server using client.send_data()
-      }
+
+      std::cout <<" === Welcome to a file storage system ===\n"<<
+                "  To create an account write '""create""'\n"<<
+                "  To connect yourself write '""connect""'\n"<<
+                "  To quit write '""quit""'"<<std::endl;
+    
+    //traiter le quit ici le reste envoyer et le serveur gère
+    std::string s;
+    while (std::cin >> s)
+    {
+        if (std::strcmp(s.c_str(),"quit")==0)
+        {
+            exit(EXIT_SUCCESS);
+        }else if (std::strcmp(s.c_str(),"create")==0 || std::strcmp(s.c_str(),"connect")==0)
+        {
+            break;
+        }
+    }
+
+    int sockfd = connect_to_server();
+
+    std::string username = ask_username();
+    char pass[72];
+    ask_password(pass);
+    user usr(username,pass);
+
+
+    //envoyer au server
+
+
+
+
+    
+
+    std::cout<<usr.get_username()<<" "<<usr.get_password()<<std::endl;
+    
+
+    read_from_connection(sockfd);
+
+
+    char buff[100];
+    while(read(1, buff, 100) > 0)
+    {
+        write(sockfd, buff, 100);
+        memset(buff, 0, sizeof(buff));
+    }
+
+    close(sockfd);
    }
+}
--- server/tlsSever.cpp
+++ server/tlsSever.cpp
@@ -14,6 +14,21 @@
 
 #include <memory>
 
+
+#include <arpa/inet.h>
+#include <sys/socket.h> // For socket functions
+#include <netinet/in.h> // For sockaddr_in
+#include <cstdlib> // For exit() and EXIT_FAILURE
+#include <iostream> // For cout
+#include <unistd.h> // For read
+#include <errno.h>
+#include <cstring>
+#include <thread>
+
+
+
+
+
 /**
  * @brief Callbacks invoked by TLS::Channel.
  *
@@ -61,8 +76,9 @@ class Callbacks : public Botan::TLS::Callbacks
 class Server_Credentials : public Botan::Credentials_Manager
 {
    public:
-      Server_Credentials(){}
-
+      Server_Credentials()// : m_key(Botan::PKCS8::load_key("botan.randombit.net.key"))
+         {
+         }
       std::vector<Botan::Certificate_Store*> trusted_certificate_authorities(
          const std::string& type,
          const std::string& context) override
@@ -80,7 +96,14 @@ class Server_Credentials : public Botan::Credentials_Manager
          {
          // return the certificate chain being sent to the tls client
          // e.g., the certificate file "botan.randombit.net.crt"
-         return {};
+         uint32_t expire_time = 60*60*24*90; //days of validity in seconds ==> 90 days
+         Botan::X509_Cert_Options opt("server/Belgium",expire_time);
+
+         Botan::AutoSeeded_RNG rng;
+         Botan::RSA_PrivateKey pKey(rng,4096);
+         const Botan::X509_Certificate cert = Botan::X509::create_self_signed_cert(opt,pKey,"SHA-256",rng);
+
+         return {Botan::X509_Certificate(cert)};
          }
 
       Botan::Private_Key* private_key_for(const Botan::X509_Certificate& cert,
@@ -98,21 +121,56 @@ class Server_Credentials : public Botan::Credentials_Manager
 
 Botan::X509_Certificate create_certificate()
 {
-   uint32_t expire_time = 60*60*24*360; //seconds, minutes, hours, days ==> in seconds
+   uint32_t expire_time = 60*60*24*90; //days of validity in seconds ==> 90 days
    Botan::X509_Cert_Options opt("server/Belgium",expire_time);
 
    Botan::AutoSeeded_RNG rng;
-   Botan::RSA_PrivateKey pKey(rng,2048,3);
+   Botan::RSA_PrivateKey pKey(rng,4096);
    Botan::X509_Certificate cert = Botan::X509::create_self_signed_cert(opt,pKey,"SHA-256",rng);
 
    return cert;
 }
 
+
+
+void threadClient(sockaddr_in sockaddr,int connection)
+{
+    // Send a message to the connection
+    std::string response = "Good talking to you\n";
+    write(connection, response.c_str(), response.size());
+
+    // Read from the connection
+    char buffer[100];
+    while (read(connection, buffer, 100)>0)
+    {
+        std::cout << "The message was: " << buffer;
+    }
+    
+    close(connection);
+
+}
+
+void command(int sockfd)
+{
+    std::string s;
+    while (std::cin >> s)
+    {
+        if (s == "quit")
+        {
+            close(sockfd);
+            exit(EXIT_FAILURE);
+        }
+    }
+}
+
+
+
+
 int main()
 {
 
-   Botan::X509_Certificate cert =  create_certificate();
-   std::cout<<cert.to_string()<<std::endl;
+   //Botan::X509_Certificate cert =  create_certificate();
+   //std::cout<<cert.to_string()<<std::endl;
 
    // prepare all the parameters
    Callbacks callbacks;
@@ -128,10 +186,72 @@ int main()
                            policy,
                            rng);
 
+   
+
    // read data received from the tls client, e.g., using BSD sockets or boost asio
    // and pass it to server.received_data().
    // ...
 
    // send data to the tls client using server.send_data()
    // ...
+
+
+      // Create a socket (IPv4, TCP)
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0); //man socket(2)
+    //AF_INET (IPv4 protocol) , AF_INET6 (IPv6 protocol)
+    //SOCK_STREAM: TCP(reliable, connection oriented) SOCK_DGRAM: UDP(unreliable, connectionless)
+    //Protocol value for Internet Protocol(IP), which is 0. This is the same number which appears on protocol field in the IP header of a packet.(man protocols for more details)
+    if (sockfd < 0)
+    {
+    std::cout << "Failed to create socket. errno: " << errno << std::endl;
+    exit(EXIT_FAILURE);
+    }
+
+    std::thread com(command, sockfd);
+    com.detach();
+
+    // Listen to port 9999 on any address
+    sockaddr_in sockaddr;                     //https://stackoverflow.com/a/21099172  https://www.gta.ufrj.br/ensino/eel878/sockets/sockaddr_inman.html
+    sockaddr.sin_family = AF_INET;
+    sockaddr.sin_port = htons(9999); // htons is necessary to convert a number to
+    sockaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+                                    // network byte order
+
+    // https://linux.die.net/man/3/setsockopt
+    /* Enable the socket to reuse the address */
+    /*
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &sockaddr, sizeof(sockaddr)) == -1) 
+    {
+        perror("setsockopt");
+        return 1;
+    }
+    */
+    if (bind(sockfd, (struct sockaddr*)&sockaddr, sizeof(sockaddr)) < 0)
+    {
+        std::cout << "Failed to bind to port 9999. errno: " << errno << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    // Start listening. Hold at most 10 connections in the queue
+    if (listen(sockfd, 10) < 0)
+    {
+        std::cout << "Failed to listen on socket. errno: " << errno << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    while (!server.is_closed())
+    {
+        // Grab a connection from the queue
+        auto addrlen = sizeof(sockaddr);
+        int connection = accept(sockfd, (struct sockaddr*)&sockaddr, (socklen_t*)&addrlen); //https://linux.die.net/man/3/accept
+        if (connection < 0) 
+        {
+            close(connection);
+            exit(EXIT_FAILURE);
+        }
+
+        std::thread test(threadClient,sockaddr,connection);
+        test.detach();
+    }
+
 }
